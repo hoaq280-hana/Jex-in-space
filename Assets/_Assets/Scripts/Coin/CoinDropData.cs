10737a25d3bc60a2060e4661403cf7351ece2343
Hoa_Hana <hoaq280@gmail.com>
2025-11-16 19:39:56 +0700

    Coin

---
+++ Assets/_Assets/Scripts/Coin/CoinDropData.cs
@@ -0,0 +1,9 @@
+
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data/CoinDrop")]
+public class CoinDropData : ScriptableObject
+{
+    public int quanityMin = 4;
+    public int quanityMax = 6;
+}
