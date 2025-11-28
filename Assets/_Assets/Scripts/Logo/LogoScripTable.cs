43a47d3d1e6d951ceb458654d3ac0f6cf0008ce5
Hoa_Hana <hoaq280@gmail.com>
2025-11-28 11:52:42 +0700

    Logo

---
+++ Assets/_Assets/Scripts/Logo/LogoScripTable.cs
@@ -0,0 +1,14 @@
+
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName =("Data/LogoScipTable"))]
+public class LogoScripTable : ScriptableObject
+{
+    public float addForce_max;
+    public float addForce_min;
+    public float addForceTorque_max;
+    public float addForceTorque_min;
+    public float timeReset;
+    public float timeStart;
+}
