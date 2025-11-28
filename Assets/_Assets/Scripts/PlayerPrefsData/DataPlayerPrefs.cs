7167a38bc3663e200c70d246f5d6913e8402bfd6
Hoa_Hana <hoaq280@gmail.com>
2025-11-28 11:55:02 +0700

    PlayerPrefsData

---
+++ Assets/_Assets/Scripts/PlayerPrefsData/DataPlayerPrefs.cs
@@ -0,0 +1,23 @@
+
+public static class DataPlayerPrefs
+{
+    // InGame
+    public static string para_TUTORIAL = "isTutorial";
+    public static string para_TOTALCOIN = "TotalCoin";
+    public static string para_HIGHWAVE = "HighWave";
+    public static string para_HIGHSCORE = "HighScore";
+    public static string para_IDGUN = "IdGun";
+    public static string para_SKILL = "Skill";
+    // Setting
+    public static string para_Setting = "Setting";
+
+    // Data
+    public static string _pathSaveData = "GunData.json";
+
+    // FirstPlay
+    public static string fistPlay = "FirstPlay";
+
+    //Count DestroyAsteroid
+    public static string para_COUNT_DESTROYASTEROID = "CountDestroyAsteroid";
+    public static string para_ACHIEVEMENTLIST = "AchievementList";
+}
