e36cab78d747926b365ae59b32e001d235477258
Hoa_Hana <hoaq280@gmail.com>
2025-12-07 11:54:44 +0700

    Login

---
+++ Assets/_Assets/Scripts/Login/Login.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using TMPro;
+
+public class Login : MonoBehaviour
+{
+    private void Start()
+    {
+        PlayGamesPlatform.Activate();
+        SignIn();
+    }
+
+    public void SignIn()
+    {
+        PlayGamesPlatform.Instance.Authenticate(ProcessAuthentication);
+    }    
+
+    internal void ProcessAuthentication(SignInStatus status)
+    {
+        if(status == SignInStatus.Success)
+        {
+            string name = PlayGamesPlatform.Instance.GetUserDisplayName();
+            Debug.Log("Login Success with name : " + name);
+        }
+        else
+        {
+            Debug.Log("Login failed : " + status.ToString());
+
+        }    
+    }    
+
+}
